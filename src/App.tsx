b840f03e1a131b1ce2928ee6933eafa1809b92b7
mintaeKim <peterzzo@naver.com>
2024-03-23 19:05:01 +0900

    header part 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
 function App() {
   return (
-    null
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
