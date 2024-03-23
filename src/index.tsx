ee23ad02e2d07ae3e4d2891446c49b9f262bf849
mintaeKim <peterzzo@naver.com>
2024-03-24 06:52:13 +0900

    home setup

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 import { createGlobalStyle } from "styled-components";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -71,7 +72,7 @@ a {
 }
 `;
 
-
+const client = new QueryClient();
 
 
 const root = ReactDOM.createRoot(
@@ -80,10 +81,12 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <QueryClientProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>
 );
